8c0db1c3cd71bf5b7641ce425450f7f41be7b664
Randal Pope <randalpope@Randals-MacBook-Air.local>
2026-03-03 10:49:44 -0500

    updated buttons

--- base.php
+++ base.php
@@ -38,7 +38,7 @@ use Roots\Sage\Wrapper;
                   <span class="screen-reader-text">Search</span>
                   <input type="search" class="search-field" id="search-field" placeholder="Search…" value="" name="s">
                 </label>
-                <button class="button"> <i class="fa-solid fa-magnifying-glass"></i></button>
+                <button class="uk-button"> <i class="fa-solid fa-magnifying-glass"></i></button>
               </form>
             </div>
 
