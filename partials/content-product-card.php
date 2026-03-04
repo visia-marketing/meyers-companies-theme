322a2e74795214a03f23172935641184b2007b0b
Randal Pope <randalpope@Randals-MacBook-Air.local>
2026-03-04 14:53:36 -0500

    added product cards

---
+++ partials/content-product-card.php
@@ -0,0 +1,24 @@
+<?php
+if( array_key_exists( 'prod_id', $args ) ){
+    $id = $args['prod_id'];
+}
+?>
+
+
+
+<div class="uk-card uk-margin-medium-bottom">
+
+    <div class="uk-card-media-top">
+        <a href="<?php echo get_the_permalink($id); ?>" >
+            <?php echo get_the_post_thumbnail($id, 'large') ?: '<div class="placeholder"></div>'; ?>
+        </a>
+    </div>
+
+    <div class="card-body">
+        <h3 class="card-title"><?php echo get_the_title($id); ?></h3>
+        <p><?php echo get_the_excerpt($id); ?></p>
+
+        <a class="uk-button uk-button-arrow uk-button-text" href="<?php echo get_the_permalink($id); ?>" >View Product</a>
+    </div>
+
+</div>
