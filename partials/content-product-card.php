5989a81cc94270fc94940f5ed72ddd435dc14a94
Randal Pope <randalpope@Randals-MacBook-Air.local>
2026-03-04 15:31:16 -0500

    updated cards

--- partials/content-product-card.php
+++ partials/content-product-card.php
@@ -9,8 +9,8 @@ if( array_key_exists( 'prod_id', $args ) ){
 <div class="uk-card uk-margin-medium-bottom">
 
     <div class="uk-card-media-top">
-        <a href="<?php echo get_the_permalink($id); ?>" >
-            <?php echo get_the_post_thumbnail($id, 'elk-product-thumb') ?: '<div class="placeholder"></div>'; ?>
+        <a href="<?php echo get_the_permalink($id); ?>" class="uk-flex uk-width-1-1">
+            <?php echo get_the_post_thumbnail($id, 'elk-product-thumb', array('class' => 'uk-width-1-1')) ?: '<div class="placeholder"></div>'; ?>
         </a>
     </div>
 
