7f9cc7cd84fb84e9ffe9a82b620cefb2a99aa4ff
Randal Pope <randalpope@Randals-MacBook-Air.local>
2026-03-05 16:15:54 -0500

    updated footer

--- partials/site-footer.php
+++ partials/site-footer.php
@@ -11,60 +11,73 @@ if ( $footer_form && (!empty($footer_form['heading']) || !empty($footer_form['ti
 <?php endif; */ ?>
 
 <footer class="main-footer">
-  <div class="uk-container uk-container-xlarge uk-flex">   
-    <div class="uk-width-1-1@xs uk-width-1-5@m">
-      <div class="footer-logo uk-margin-medium-right">
+
+  <div class="uk-container uk-container-xlarge uk-flex uk-flex-wrap">   
+
+    <div class="uk-width-1-1@s uk-width-1-1@m uk-width-1-2@xl uk-margin-medium-bottom uk-margin-remove-bottom@m">
+      <div class="footer-logo">
         <a href="<?= esc_url(home_url('/')); ?>"><img src="<?php the_field('footer_logo', 'option');?>" alt="<?php bloginfo('name'); ?>"></a>
       </div>
     </div>
-    <div class="uk-width-1-1@xs uk-width-1-5@m ">   
-      <?php
-      if (has_nav_menu('footer_navigation_1')) :
-      wp_nav_menu(['theme_location' => 'footer_navigation_1', 'depth' => 2, 'menu_class' => 'footer-menu uk-margin-medium-left uk-margin-medium-right' ]); 
-      endif;
-      ?>
-    </div>
-    <div class="uk-width-1-1@xs uk-width-1-5@m">   
-      <?php
-      if (has_nav_menu('footer_navigation_2')) :
-      wp_nav_menu(['theme_location' => 'footer_navigation_2', 'depth' => 2, 'menu_class' => 'footer-menu uk-margin-medium-left uk-margin-medium-right' ]); 
-      endif;
-      ?>
-    </div>
-    <div class="uk-width-1-1@xs uk-width-1-5@m ">   
-      <?php
-      if (has_nav_menu('footer_navigation_3')) :
-      wp_nav_menu(['theme_location' => 'footer_navigation_3', 'depth' => 2, 'menu_class' => 'footer-menu uk-margin-medium-left uk-margin-medium-right' ]); 
-      endif;
-      ?>
-    </div>
-    <div class="uk-width-1-1@xs uk-width-1-5@m">
-      <div class="uk-margin-medium-left uk-margin-medium-right">
-        <?php echo get_field('footer_contact', 'options');?>
-      </div>
-    </div>
-  </div>
-  <div class="uk-container  uk-container-xlarge uk-margin-top">
-    <?php if( have_rows('footer_badges', 'options') ): ?>
-      <div class="footer-badges">
-          <?php while (have_rows('footer_badges', 'options')): the_row(); 
-              $image = get_sub_field('badge_image', 'options');
-              $text = get_sub_field('badge_text', 'options');
-              $url = get_sub_field('badge_url', 'options');
-              ?>
-              <div class="badge">
-                <?php if($url): echo '<a href="' . esc_url($url) . '">'; endif;?>
-                  <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-                <?php if($url): echo '</a>'; endif;?>
-                <?php if($url): echo '<a href="' . esc_url($url) . '">'; endif;?>
-                  <p><?php echo esc_html($text); ?></p>
-                <?php if($url): echo '</a>'; endif;?>
-              </div>
-          <?php endwhile; ?>
-      </div>
-    <?php endif; ?>
-  </div>
-  <div class="uk-container  uk-container-xlarge uk-margin-top">
+
+    <div class="uk-width-1-1@s uk-width-1-1@m uk-width-1-2@xl  uk-margin-medium-bottom uk-margin-remove-bottom@m">
+      <div class="uk-grid uk-grid-small">
+
+        <div class="uk-width-1-1@s uk-width-1-4@m ">   
+          <?php
+          if (has_nav_menu('footer_navigation_1')) :
+          wp_nav_menu(['theme_location' => 'footer_navigation_1', 'depth' => 2, 'menu_class' => 'footer-menu' ]); 
+          endif;
+          ?>
+        </div>
+
+        <div class="uk-width-1-1@s uk-width-1-4@m">   
+          <?php
+          if (has_nav_menu('footer_navigation_2')) :
+          wp_nav_menu(['theme_location' => 'footer_navigation_2', 'depth' => 2, 'menu_class' => 'footer-menu ' ]); 
+          endif;
+          ?>
+        </div>
+
+        <div class="uk-width-1-1@s uk-width-1-4@m ">   
+          <?php
+          if (has_nav_menu('footer_navigation_3')) :
+          wp_nav_menu(['theme_location' => 'footer_navigation_3', 'depth' => 2, 'menu_class' => 'footer-menu ' ]); 
+          endif;
+          ?>
+        </div>
+
+        <div class="uk-width-1-1@s uk-width-1-4@m">
+          <div class="uk-flex uk-flex-column uk-flex-middle uk-flex-top@m">
+            <p class="connect">Connect with Us</p>
+
+            <?php $social_media = get_field('social_media', 'options'); ?>
+            <div class="social-icons uk-margin-small-top uk-flex uk-flex-wrap uk-flex-left">
+              <?php foreach( $social_media as $social ): ?>
+                <a href="<?php echo $social['url']; ?>" target="_blank" class="uk-margin-small-right">
+                  <i class="fa-brands fa-<?php echo $social['social_icon']; ?> fa-2xl"></i>
+                </a>
+              <?php endforeach; ?>
+            </div>
+
+            <?php $footer_badges = get_field('footer_badges', 'options'); ?>
+            <div class="badges uk-margin-medium-top uk-flex uk-flex-wrap uk-flex-left">
+              <?php foreach( $footer_badges as $badge ): ?>
+                <a href="<?php echo $badge['badge_url']; ?>" target="_blank" class="uk-width-small uk-margin-small-right">
+                  <?php echo wp_get_attachment_image( $badge['badge_image'], 'medium' , false, array('class' => 'footer-badge') ); ?>
+                </a>
+              <?php endforeach; ?>
+            </div>
+            
+        </div>
+
+      </div> <!-- .uk-grid -->
+    </div><!-- .uk-width-1-2 -->
+
+    
+  </div> <!-- uk container -->
+  
+  <div class="uk-container uk-container-xlarge uk-margin-xlarge-top uk-flex uk-flex-left uk-width-1-1 copyright-container">
     <div class="footer-copyright">
       <div class="copyright">
         <?php echo get_field('copyright', 'options');?>
@@ -76,11 +89,5 @@ if ( $footer_form && (!empty($footer_form['heading']) || !empty($footer_form['ti
       ?>
     </div>
   </div>
-  <div class="uk-container  uk-container-xlarge">
-    <div class="small-12 columns">
-      <strong class="footer-tagline">
-        <?php echo get_field('footer_tagline', 'options');?>
-      </strong>
-    </div>
-  </div>
+
 </footer>
